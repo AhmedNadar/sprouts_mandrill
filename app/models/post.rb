e660a0d3ad0c18f9a060a16e4bfe4330c34fd2ed
Ahmed <ahmedfay@hotmail.com>
2015-02-12 15:28:34 -0500

    Update associations and validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ActiveRecord::Base
+
+  # associations
+  belongs_to :user
+
+  # validation
+  default_scope -> { order(created_at: :desc) }
+  validates :title, presence: true
+  validates :content, presence: true, length: { maximum: 140 }
 end
